b2a8286d3cb5f68baf323ea8b76439f1ca6abd0b
atha <athabarokahfc@gmail.com>
2024-03-27 17:37:27 +0700

    create: initialize folder configuration

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className=''>
+        <div>
+            
+        </div>
+        <div >
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
