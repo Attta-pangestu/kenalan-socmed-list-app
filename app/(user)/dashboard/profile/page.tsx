790501f6199bcfb09884ea3b1949aefe4baf1136
atha <athabarokahfc@gmail.com>
2024-03-31 23:32:02 +0700

    create: sign-in and sign-up integration with clerk

--- app/(user)/dashboard/profile/page.tsx
+++ app/(user)/dashboard/profile/page.tsx
@@ -6,6 +6,7 @@ import { Label } from '@/components/ui/label'
 import { Skeleton } from '@/components/ui/skeleton'
 import Image from 'next/image'
 import Link from 'next/link'
+import { ClerkLoaded, ClerkLoading, currentUser } from '@clerk/nextjs'
 
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -15,12 +16,6 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 
-const currentUser = {
-    imageUrl: 'https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18yZUdKZmhiR1dWT2QzWFJrMkxuMGFwcUFsb04ifQ', 
-    emailAddress: 'athabarokah@gmail.com',
-    firstName: 'Atha Rizki', 
-    lastName: 'Pangestu'
-}
 
 
 const ProfilImageElement = ({imageUrl} : {imageUrl: string}) => {
@@ -48,20 +43,47 @@ const InputLabelElement = ({label, id,  type, value} : {label: string, id: strin
 
 
 
-const page = () => {
-    const { imageUrl, emailAddress, firstName, lastName } = currentUser;
-
+const page = async () => {
+    const clerkCurrentUser : any = await currentUser();
+    const { imageUrl, emailAddresses, firstName, lastName } = clerkCurrentUser;
     return (
         <div className='flex flex-col items-center justify-center w-full '>
-            <ProfilImageElement imageUrl={imageUrl} />
-            <div className='mt-8 w-1/2 grid gap-2 border border-white shadow-md shadow-neutral-700 rounded-lg p-5'>
-                <InputLabelElement label="Email Address" id="emailAddress" type="email" value={emailAddress} />
-                <InputLabelElement label="First Name" id="firstName" type="text" value={firstName} />
-                <InputLabelElement label="Last Name" id="lastName" type="text" value={lastName} />
-                <Button asChild>
-                    <Link href='/user-profile' >Tap To Edit</Link>
-                </Button>
-            </div>
+            <ClerkLoaded>
+                <ProfilImageElement imageUrl={imageUrl} />
+            </ClerkLoaded>
+            <ClerkLoaded>
+                <div className='mt-8 w-1/2 grid gap-2 border border-white shadow-md shadow-neutral-700 rounded-lg p-5'>
+                    <InputLabelElement label="Email Address" id="emailAddress" type="email" value={emailAddresses[0].emailAddress}  />
+                    <InputLabelElement label="First Name" id="firstName" type="text" value={firstName} />
+                    <InputLabelElement label="Last Name" id="lastName" type="text" value={lastName} />
+                    <Button asChild>
+                        <Link href='/user-profile' >Tap To Edit</Link>
+                    </Button>
+                </div>
+            </ClerkLoaded>
+            <ClerkLoading>
+                    <div className='flex flex-col items-center justify-center gap-2 mt-10'>
+                       <Skeleton className='w-20 h-20 rounded-full' />
+                       <Skeleton className='w-24 h-5' />
+                    </div>
+
+                    <div className='mt-8 w-1/2 grid gap-2 border border-white shadow-md shadow-neutral-700 rounded-lg p-5'>
+                    <Skeleton className='h-8' />
+                    <Skeleton className='h-10' />
+                    <Skeleton className='h-8' />
+                    <Skeleton className='h-10' />
+                    <Skeleton className='h-8' />
+                    <Skeleton className='h-10' />
+                    <Button asChild>
+                        <Link href='/user-profile'>
+                            <Skeleton className='h-10 w-full' />
+                        </Link>
+                    </Button>
+    </div>
+
+
+            </ClerkLoading>
+
         
         </div>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
                 showAtBottom={false}
               />
           <div className="absolute inset-0 -z-10 h-[600px] w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:40px_40px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary opacity-10 dark:opacity-25 blur-[100px]"></div></div>
-            <main className="py-[22px] px-10 md:px-28 md:py-12">
+            <main className="flex flex-col justify-center items-center py-[22px] px-10 md:px-28 md:py-12">
               {children}
             </main>
           </ThemeProvider>
